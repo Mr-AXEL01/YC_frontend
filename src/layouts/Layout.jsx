1bc98aae2b508be4c08238f3e72e18730a98b22c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-06 17:00:08 +0000

    set the link to all the pages

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,33 +1,56 @@
-import {Outlet} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 
 export default function Layout() {
-  return (
-    <>
-      <header>
-        <div className="items-center justify-between flex bg-gray-800 bg-opacity-90 px-12 py-4 mb-4 mx-auto shadow-2xl">
-          <div className="text-2xl text-white font-semibold inline-flex items-center">
-            <img src="#" className="w-16 mr-4" alt="logo-img" />
-            <span>YouCare</span>
-          </div>
-          <div>
-            <ul>
-              <li className="ml-5 px-3 py-1">
-                <Link to={'/'}>Home</Link>
-              </li>
-              <li className="ml-5 px-3 py-1"><a href="#">Login</a></li>
-              <li className="ml-5 px-3 py-1"><a href="#">Register</a></li>
-              <li className="ml-5 px-3 py-1"><a href="#">Users</a></li>
-              <li className="ml-5 px-3 py-1 rounded font-semibold bg-gray-100 text-gray-800"><a href="#">change mode</a></li>
-            </ul>
-          </div>
-        </div>
-      </header>
+    return (
+        <>
+            <header>
+                <div
+                    className="items-center justify-between flex bg-gray-800 bg-opacity-90 px-12 py-4 mb-4 mx-auto shadow-2xl">
+                    <Link to={'/'}>
+                        <div className="text-2xl text-white font-semibold inline-flex items-center">
+                            <img
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzgeVnTqHCBfIny-LQCUcnx9aOellsv6AxpSdht7X9mMIp0jRQIe_7uT7PbjKoAg-zdgk&usqp=CAU"
+                                className="w-12 mr-4 rounded-[20px]" alt="logo-img"/>
+                            <span>YouCare</span>
+                        </div>
+                    </Link>
+                    <div>
+                        <ul className="flex text-white">
+                            <li className="ml-5 px-3 py-1">
+                                <Link to={'/'}>
+                                    Home
+                                </Link>
+                            </li>
+                            <li className="ml-5 px-3 py-1">
+                                <Link to={'/login'}>
+                                    Login
+                                </Link>
+                            </li>
+                            <li className="ml-5 px-3 py-1">
+                                <Link to={'/register'}>
+                                    Register
+                                </Link>
+                            </li>
+                            <li className="ml-5 px-3 py-1">
+                                <Link to={'/users'}>
+                                    Users
+                                </Link>
+                            </li>
+                            <li className="ml-5 px-3 py-1 rounded font-semibold bg-gray-100 text-gray-800">
+                                <Link to={'/'}>
+                                    Change mode
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </header>
 
-      <main>
-        <Outlet/>
-      </main>
+            <main>
+                <Outlet/>
+            </main>
 
-      <footer>Footer</footer>
-    </>
-  );
+            <footer>Footer</footer>
+        </>
+    );
 }
