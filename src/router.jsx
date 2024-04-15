c89fa086132cdf6cb19c0e5b236eb59dd18f113e
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-15 16:09:11 +0100

    create route for layouts and set childrens

--- src/router.jsx
+++ src/router.jsx
@@ -2,21 +2,35 @@ import {createBrowserRouter} from "react-router-dom";
 import Dashboard from "@/views/Dashboard.jsx";
 import Login from "@/views/Login.jsx";
 import Register from "@/views/Register.jsx";
+import GuestLayout from "@/components/GuestLayout.jsx";
+import DefaultLayout from "@/components/DefaultLayout.jsx";
 
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Dashboard />
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/dashboard',
+                element: <Dashboard />
+            }
+        ]
     },
     {
-        path:'/login',
-        element: <Login />
-    },
-    {
-        path:'/register',
-        element: <Register />
-    },
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path:'/login',
+                element: <Login />
+            },
+            {
+                path:'/register',
+                element: <Register />
+            },
+        ]
+    }
 ])
 
 export default router;
